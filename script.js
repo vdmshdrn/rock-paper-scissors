10859032bbcba1d3833a66f7d3535fc734f5e347
Vadim <vadimshdrn@gmail.com>
2023-01-26 22:26:00 +0600

    Declare getComputerChoise

---
+++ script.js
@@ -0,0 +1,6 @@
+function getComputerChoise() {
+    let getRandomNumber = Math.floor(1 + Math.random() * (3 + 1 -1));
+    //return getRandomNumber;
+    console.log(getRandomNumber);
+}
+getComputerChoise();
