b56efcc365158fb871022f876698df8054a087e7
Vadim <vadimshdrn@gmail.com>
2023-01-26 22:32:02 +0600

    Function getComputerChoise returns random computer choise

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function getComputerChoise() {
     let getRandomNumber = Math.floor(1 + Math.random() * (3 + 1 -1));
     //return getRandomNumber;
-    console.log(getRandomNumber);
+    
+    if (getRandomNumber === 1) {
+        console.log('Rock');
+    } else if (getRandomNumber === 2) {
+        console.log ('Paper');
+    } else {
+        console.log ('Scissors');
+    }
 }
 getComputerChoise();
